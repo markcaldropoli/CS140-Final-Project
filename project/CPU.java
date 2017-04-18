e35b1395b71f068fb603e2c0fc3a7f772d5af9fa
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-04-18 10:37:46 -0400

    Create CPU.java

---
+++ project/CPU.java
@@ -0,0 +1,35 @@
+package project;
+
+public class CPU {
+	private int accum;
+	private int pCounter;
+	private int memBase;
+	
+	public int getAccum() {
+		return accum;
+	}
+	
+	public int getPCounter() {
+		return pCounter;
+	}
+	
+	public int getMemBase() {
+		return memBase;
+	}
+	
+	public void setAccum(int ac) {
+		accum = ac;
+	}
+	
+	public void setPCounter(int pc) {
+		pCounter = pc;
+	}
+	
+	public void setMemBase(int mb) {
+		memBase = mb;
+	}
+	
+	public void incPC() {
+		pCounter++;
+	}
+}
