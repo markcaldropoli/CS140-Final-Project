5adfa251b35b474d5388a540bb0015f914a05c07
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-04-19 11:08:18 -0400

    Add files via upload

--- src/project/MachineModel.java
+++ src/project/MachineModel.java
@@ -10,6 +10,21 @@ public class MachineModel {
 	
 	public MachineModel(HaltCallback callback) {
 		this.callback = callback;
+		
+		//INSTRUCTION MAP entry for "ADD"
+		IMAP.put(0x3, (arg, level) -> {
+			if(level < 0 || level > 2) {
+				throw new IllegalArgumentException(
+					"Illegal indirection level in ADD instruction");
+			}
+			if(level > 0) {
+				IMAP.get(0x3).execute(memory.getData(cpu.getMemBase()+arg), level-1);
+			} else {
+				cpu.setAccum(cpu.getAccum() + arg);
+				cpu.incrPC();
+			}
+		});
+		
 	}
 	
 	public MachineModel() {
