9bf15eb8191023d08fe5670f3149394302a9ff09
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-04-24 00:46:10 -0400

    Create GUIMediator

---
+++ src/project/GUIMediator.java
@@ -0,0 +1,31 @@
+package project;
+
+import javax.swing.JFrame;
+
+public class GUIMediator {
+	private MachineModel model;
+	
+	public MachineModel getModel() {
+		return model;
+	}
+	
+	public void setModel(MachineModel model) {
+		this.model = model;
+	}
+	
+	public void step() {
+		
+	}
+	
+	public JFrame getFrame() {
+		return null;
+	}
+	
+	void clearJob() {
+		
+	}
+	
+	void makeReady(String s) {
+		
+	}
+}
