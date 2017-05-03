fdb691154783e30ff99e97a5197b95e5f4583a88
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-05-02 20:08:07 -0400

    Part 10

--- src/project/Code.java
+++ src/project/Code.java
@@ -74,6 +74,20 @@ public class Code {
 		return bldr.toString();
 	}
 	
+	public String getHex(int i) {
+		int a = getOp(i);
+		int b = getIndirLvl(i);
+		int c = getArg(i);
+		if(c < 0) {
+			return Integer.toHexString(a).toUpperCase()+" "+
+					Integer.toHexString(b).toUpperCase()+" -"+
+					Integer.toHexString(-c).toUpperCase();
+		}
+		return Integer.toHexString(a).toUpperCase()+" "+
+				Integer.toHexString(b).toUpperCase()+" "+
+				Integer.toHexString(c).toUpperCase();
+	}
+	
 	/*public static void main(String[] args) {
 		Code c = new Code();
 		for(int i = 0; i <= 3; i++) {
