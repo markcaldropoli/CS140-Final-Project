8ce74e50e865e6515b488a258b0ebc9e68c99761
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-05-04 10:56:27 -0400

    Create ProcessorViewPanel

---
+++ src/project/ProcessorViewPanel.java
@@ -0,0 +1,55 @@
+package project;
+
+import java.awt.GridLayout;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ProcessorViewPanel implements Observer {
+	private MachineModel model;
+	private JTextField acc = new JTextField();
+	private JTextField pc = new JTextField();
+	private JTextField mb = new JTextField();
+
+	public ProcessorViewPanel(GUIMediator gui, MachineModel mdl) {
+		model = mdl;
+		gui.addObserver(this);
+	}
+
+	public JComponent createProcessorDisplay() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1,0));
+		panel.add(new JLabel("Accumulator: "));
+		panel.add(acc);
+		panel.add(new JLabel("Program Counter: "));
+		panel.add(pc);
+		panel.add(new JLabel("Memory Base: "));
+		panel.add(mb);
+		return panel;
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		if(model != null) {
+			acc.setText("" + model.getAccum());
+		}
+	}
+	
+	public static void main(String[] args) {
+		GUIMediator view = new GUIMediator(); 
+		MachineModel model = new MachineModel();
+		ProcessorViewPanel panel = 
+			new ProcessorViewPanel(view, model);
+		JFrame frame = new JFrame("TEST");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(700, 60);
+		frame.setLocationRelativeTo(null);
+		frame.add(panel.createProcessorDisplay());
+		frame.setVisible(true);
+	}
+}
